0574d654ffd7f3f19d7ff08e8a6cc773f83d302d
Pål Næverlid Sævik <paal.naeverlid.saevik@hi.no>
2022-12-08 08:10:05 +0100

    Add depth functions

--- romsviz/functions.py
+++ romsviz/functions.py
@@ -17,3 +17,25 @@ def open_roms(paths):
         join='override',
         combine_attrs='override',
     )
+
+
+def add_zrho(dset):
+    if dset.Vtransform == 1:
+        z_rho = dset.hc * (dset.s_rho - dset.Cs_r) + dset.Cs_r * dset.h
+    elif dset.Vtransform == 2:
+        z_rho = dset.h * (dset.hc * dset.s_rho + dset.Cs_r * dset.h) / (dset.hc + dset.h)
+    else:
+        raise ValueError(f"Unknown Vtransform: {dset.Vtransform}")
+
+    return dset.assign(z_rho=z_rho.transpose('s_rho', 'eta_rho', 'xi_rho'))
+
+
+def add_zw(dset):
+    if dset.Vtransform == 1:
+        z_w = dset.hc * (dset.s_w - dset.Cs_w) + dset.Cs_w * dset.h
+    elif dset.Vtransform == 2:
+        z_w = dset.h * (dset.hc * dset.s_w + dset.Cs_w * dset.h) / (dset.hc + dset.h)
+    else:
+        raise ValueError(f"Unknown Vtransform: {dset.Vtransform}")
+
+    return dset.assign(z_rho=z_w.transpose('s_w', 'eta_rho', 'xi_rho'))
--- tests/test_functions.py
+++ tests/test_functions.py
@@ -1,10 +1,18 @@
+import pytest
 from pathlib import Path
 from romsviz import functions
+import xarray as xr
 
 
 FORCING_1 = Path(__file__).parent / 'forcing_1.nc'
 FORCING_2 = Path(__file__).parent / 'forcing_2.nc'
 FORCING_STAR = str(Path(__file__).parent / 'forcing_*.nc')
+
+
+@pytest.fixture(scope='module')
+def forcing1():
+    with xr.open_dataset(FORCING_1) as dset:
+        yield dset
 
 
 class Test_open_roms:
@@ -16,3 +24,15 @@ class Test_open_roms:
         with functions.open_roms(FORCING_STAR) as dset:
             assert dset.h.dims == ('eta_rho', 'xi_rho')
             assert dset.s_rho.dims == ('s_rho', )
+
+
+class Test_add_zrho:
+    def test_zrho_has_correct_dimensions(self, forcing1):
+        dset = functions.add_zrho(forcing1)
+        assert dset.z_rho.dims == ('s_rho', 'eta_rho', 'xi_rho')
+
+
+class Test_add_zw:
+    def test_zw_has_correct_dimensions(self, forcing1):
+        dset = functions.add_zw(forcing1)
+        assert dset.z_rho.dims == ('s_w', 'eta_rho', 'xi_rho')
